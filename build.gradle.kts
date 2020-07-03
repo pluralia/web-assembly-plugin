7dfd3c98dddc665c1ed27412963ffe0d3ef00d54
Pluralia <irina.artemeva@jetbrains.com>
2020-07-03 12:42:16 +0300

    Init plugin

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.21"
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.jetbrains.webstorm"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "IU-2020.1.2"
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
