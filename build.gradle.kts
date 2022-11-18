f389dc29761de3bf8b51794efeca205285746549
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2022-11-18 15:28:48 +0200

    support 2022.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
-import org.jetbrains.grammarkit.tasks.GenerateLexer
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    id("org.jetbrains.intellij") version "1.5.2"
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.grammarkit") version "2021.1.3"
+    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.grammarkit") version "2022.3"
 }
 
 group = "org.jetbrains.webstorm"
-version = "1.4.222"
+version = "1.4.223"
 
 repositories {
     mavenCentral()
@@ -19,13 +16,17 @@ intellij {
     version.set("IU-LATEST-EAP-SNAPSHOT")
 }
 
+sourceSets {
+    main {
+        java.srcDirs("src/main/gen")
+    }
+}
+
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
     }
 
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
     }
 
     test {
@@ -33,29 +34,9 @@ tasks {
     }
 }
 
-sourceSets {
-    main {
-        java.srcDirs("src/main/gen")
-    }
-}
-
-val generateWebAssemblyLexer = task<GenerateLexer>("generateWebAssemblyLexer") {
-    source = "src/main/grammars/WebAssemblyLexer.flex"
-    targetDir = "src/main/gen/org/jetbrains/webstorm/lang/lexer"
-    targetClass = "_WebAssemblyLexer"
-    purgeOldFiles = true
-}
-
-
 tasks {
     patchPluginXml {
-        sinceBuild.set("222.0")
-        untilBuild.set("222.*")
-    }
-
-    withType<KotlinCompile> {
-        dependsOn(
-            generateWebAssemblyLexer
-        )
+        sinceBuild.set("223.0")
+        untilBuild.set("223.*")
     }
 }
